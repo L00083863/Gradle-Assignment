c39765016446308a3c00d427e66bd28d8fb7ab68
Matthew Mc Colgan <Matthew Mc Colgan@DESKTOP-0LR9QDO>
2017-04-27 18:30:38 +0100

    added postcode to address class

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -1,11 +1,13 @@
 public class Address {
+	protected Postcode postcode;
 	protected Team stadiumName;
 	protected String streetName;
 	protected String town;
 	protected String city;
 	protected String country;
 	
-	public Address(Team staidumName, String streetName, String town, String city, String country){
+	public Address(Postcode postcode, Team staidumName, String streetName, String town, String city, String country){
+		this.postcode = postcode;
 		this.stadiumName = staidumName;
 		this.streetName = streetName;
 		this.town = town;
