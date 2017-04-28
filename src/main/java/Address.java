94776554a44ad056c3369ea619a03e350311378a
Matthew Mc Colgan <Matthew Mc Colgan@DESKTOP-0LR9QDO>
2017-04-28 01:50:28 +0100

    Added and tested exceptions in address

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -14,45 +14,72 @@ public class Address {
 	}
 	
 	
+	public static boolean validateStreetName(String streetName){
+		return streetName.matches("[a-zA-Z]+([ ][a-zA-Z]+)*");
+	}
+	
+	public static boolean validateTown(String town){
+		return town.matches("[a-zA-Z]+([ '-][a-zA-Z]+)+([ '-][ ][a-zA-Z]+)*");
+	}
+	
+	public static boolean validateCity(String city){
+		return city.matches("[a-zA-Z]+([ -][ &a-zA-Z]+)+([ -][a-zA-Z]+)*");
+	}
+	
+	public static boolean validateCountry(String country){
+		return country.matches("[a-zA-Z]+([ ][a-zA-Z]+)*");
+	}
+	
 	
 	protected Team getStadiumName(){
 		return stadiumName;
-	}
-	
-	protected void setStadiumName(Team stadiumName){
-		this.stadiumName = stadiumName;
 	}
 	
 	protected String getStreetName(){
 		return streetName;
 	}
 	
-	protected void setStreetName(String streetName){
-		this.streetName = streetName;
+	protected void setStreetName(String streetName) {
+		if(validateStreetName(streetName) == true){
+			this.streetName = streetName;
+		}
+		else
+			throw new IllegalArgumentException("Street name can contain letters.");
 	}
 	
 	protected String getTown(){
 		return town;
 	}
 	
-	protected void setTown(String town){
-		this.town = town;
-	}
-	
+	protected void setTown(String town) {
+		if(validateTown(town) == true){
+			this.town = town;
+		}
+		else
+			throw new IllegalArgumentException("Town name can only contain letters, hyphens or apostrophes.");
+	}	
 	protected String getCity(){
 		return city;
 	}
 	
-	protected void setCity(String city){
-		this.city = city;
+	protected void setCity(String city) {
+		if(validateCity(city) == true){
+			this.city = city;
+		}
+		else
+			throw new IllegalArgumentException("City name can only contain &, letters or hyphens.");
 	}
 	
 	protected String getCountry(){
 		return country;
 	}
 	
-	protected void setCountry(String country){
-		this.country = country;
+	protected void setCountry(String country) {
+		if(validateCountry(country) == true){
+			this.country = country;
+		}
+		else
+			throw new IllegalArgumentException("Country name can only contain letters.");
 	}
 		
 }
--- src/test/java/AddressTest.java
+++ src/test/java/AddressTest.java
@@ -1,7 +1,9 @@
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class AddressTest {
 	private Address stadiumAddress;
@@ -13,23 +15,7 @@ public class AddressTest {
 	public void setUpAddress(){
 		stadiumAddress = new Address(postcode, stadiumName, "High Road", "Tottenham", "London", "England");
 	}
-	
-	public static boolean validateStreetAddress(String streetName){
-		return streetName.matches("[a-zA-Z0-9]+([ '-][a-zA-Z0-9]+)*");
-	}
-	
-	public static boolean validateTown(String town){
-		return town.matches("[a-zA-Z]+([ '-][a-zA-Z]+)*");
-	}
-	
-	public static boolean validateCity(String city){
-		return city.matches("[a-zA-Z]+([ '-][a-zA-Z]+)*");
-	}
-	
-	public static boolean validateCountry(String country){
-		return country.matches("[a-zA-Z]+([ '-][a-zA-Z]+)*");
-	}
-	
+
 	@Test
 	public void testGetStadiumName(){
 		assertThat(stadiumAddress.getStadiumName()).isEqualTo(stadiumName);
@@ -55,28 +41,97 @@ public class AddressTest {
 		assertThat(stadiumAddress.getCountry()).contains("land");
 	}
 	
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void testSetStadiumName(){
 		Team stadiumAddress = new Team("Parkhead");
 	}
 	
 	@Test
-	public void testSetStreetAddress(){
+	public void testSetStreetName(){
 		stadiumAddress.setStreetName("Tollcross Rd");
 	}
 	
 	@Test
-	public void testSetTown(){
-		stadiumAddress.setTown("Gallowgate");
+	public void testSetInvalidStreetName(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Street name can contain letters");
+		stadiumAddress.setStreetName("123 Baker Street");
 	}
 	
 	@Test
-	public void testSetCity(){
-		stadiumAddress.setCity("Glasgow");
+	public void testSetInvalidStreetName2(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Street name can contain letters");
+		stadiumAddress.setStreetName("Baker' Street");
+	}
+	
+	@Test
+	public void testSetInvalidStreetName3(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Street name can contain letters");
+		stadiumAddress.setStreetName("Baker-Street");
+	}
+	
+	@Test
+	public void testSetHyphenedTownName(){
+		stadiumAddress.setTown("Berwick-upon-Tweed");
+	}
+	
+	@Test
+	public void testSetApostropheTownName(){
+		stadiumAddress.setTown("Bishop's Castle");
+	}
+	
+	@Test
+	public void testSetInvalidTownName(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Town name can only contain letters, hyphens or apostrophes");
+		stadiumAddress.setTown("123 Baker Street");
+	}
+	
+	@Test
+	public void testSetCityName(){
+		stadiumAddress.setCity("Brighton & Hove");
+	}
+	
+	@Test
+	public void testSetHyphenedCityName(){
+		stadiumAddress.setCity("Stoke-on-Trent");
+	}
+	
+	@Test
+	public void testSetInvalidCityNameWithNumbers(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("City name can only contain &, letters or hyphens.");
+		stadiumAddress.setCity("123 Baker Street");
+	}
+	
+	@Test
+	public void testSetInvalidCityNameWithApostrophe(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("City name can only contain &, letters or hyphens.");
+		stadiumAddress.setCity("Liver'pool");
 	}
 	
 	@Test
 	public void testSetCountry(){
 		stadiumAddress.setCountry("Scotland");
+	}
+	
+	@Test
+	public void testSetInvalidCountryNameWithSymbol(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Country name can only contain letters.");
+		stadiumAddress.setCountry("Trinidad & Tobago");
+	}
+	
+	@Test
+	public void testSetInvalidCountryNameWithApostrophe(){
+		thrown.expect(IllegalArgumentException.class);
+		thrown.expectMessage("Country name can only contain letters.");
+		stadiumAddress.setCountry("Norn'Areland");
 	}
 }
